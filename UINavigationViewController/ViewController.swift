dcb7dc845c8209f27fb0d8d08f398a8885e21d32
SergeyGolenko <1989sergiotay@gmail.com>
2019-07-30 23:57:52 +0300

    Initial Commit

---
+++ UINavigationViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UINavigationViewController
+//
+//  Created by Sergey Golenko on 30/07/2019.
+//  Copyright © 2019 Sergey Golenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
