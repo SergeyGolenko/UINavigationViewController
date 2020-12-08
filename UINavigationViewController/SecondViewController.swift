79fdea9884afcd64837c137b9750be46ef327ca5
Sergey Golenko <50142057+SergeyGolenko@users.noreply.github.com>
2020-12-08 11:11:47 +0200

    delete SecondVC

--- UINavigationViewController/SecondViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SecondViewController.swift
-//  UINavigationViewController
-//
-//  Created by Sergey Golenko on 03/08/2019.
-//  Copyright © 2019 Sergey Golenko. All rights reserved.
-//
-
-import Foundation
